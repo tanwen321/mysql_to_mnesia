0625c053fcde2ecbba87c9c7cddc35b69dcea06a
tanwen321 <254392724@qq.com>
2019-04-17 16:18:59 +0800

    Update m2m.hrl

--- include/m2m.hrl
+++ include/m2m.hrl
@@ -2,8 +2,8 @@
 -define(DBCONN, mysql_conn).						%mysql数据库连接标识
 -define(TIMEOUT, 5000).							%mysql数据库连接超时时间
 %%mysql db info
--define(DBSERVER, "192.168.111.211").					%%mysql数据库ip
--define(DPORT, 3306).							%%mysql数据库端口
--define(DBUSER, "root").					%%mysql数据库用户名
--define(DBPASS, "admin@123").						%%mysql数据库密码
+-define(DBSERVER, "192.168.XXX.XX").					%%mysql数据库ip
+-define(DPORT, XXX).							%%mysql数据库端口
+-define(DBUSER, "XXXX").					%%mysql数据库用户名
+-define(DBPASS, "XXXXX").						%%mysql数据库密码
 -define(INFODATA, "performance_schema").				%%mysql数据库实例数据库名
